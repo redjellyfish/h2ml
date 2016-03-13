d94b41e96baa64cee05da5ce80cced539f67771d
redjellyfish.co.uk <dev@redjellyfish.co.uk>
2016-03-13 10:28:33 +0000

    Added text interpolation

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-﻿console.log('Hello world');
+﻿"use strict";
+
+let CheerioDomHelper = require("./lib/CheerioDomHelper");
+let Engine = require("./lib/engine");
+
+module.exports = {
+    compile: function (html, options, model) {
+        let engine = new Engine(options);
+        return engine.compile(html, model);
+    },
+    render: function (html, model, options) {
+        let engine = new Engine(options);
+        return engine.render(html, model);
+    }
+}
---
+++ test/engine.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+let expect = require("chai").expect;
+let h2ml = require("../app");
+
+describe("Engine", function () {
+    describe("Text interpolation", function () {
+        it("can pull values from the model", function () {
+            h2ml.render("${model.text}", { text: "hello world" }).then(html => {
+                expect(html).to.equal("hello world");
+            });
+        });
+
+        it("html encodes output by default");
+
+        it("escapes any characters that would break syntax generation");
+    });
+});
