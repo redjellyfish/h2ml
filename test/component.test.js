f73c89ac7f691345b9162506c4856c287dbdbfe3
redjellyfish.co.uk <dev@redjellyfish.co.uk>
2016-03-15 07:59:36 +0000

    Started component

---
+++ test/component.test.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+var chai = require("chai");
+let expect = chai.expect;
+var chaiAsPromised = require("chai-as-promised");
+let h2ml = require("../app");
+let entities = new (require('html-entities').XmlEntities)();
+
+chai.use(chaiAsPromised);
+
+describe("Component", function () {
+
+    it("element requires ID attribute", function () {
+        let input = '<component></component>';
+        return expect(h2ml.render(input, { message: "hello world" })).to.be.rejected;
+    });
+
+    it("element is removed from the dom", function () {
+        let input = 'a<component id="test"></component>b';
+        return expect(h2ml.render(input)).to.eventually.equal('ab');
+    });
+
+});
